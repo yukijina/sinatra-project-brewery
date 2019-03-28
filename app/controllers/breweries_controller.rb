98529b7bdadf8761336c8f3cb6528306af33293b
yukijina <yukijina@gmail.com>
2019-03-27 22:48:06 -0700

    create new route for signup and login

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,4 +1,30 @@
 class BreweriesController < ApplicationController
 
+  get "/signup" do
+    erb :"/breweries/new"
+  end
+
+  post "/signup" do
+    binding.pry
+    brewery = Brewery.new(params)
+    if brewery.save
+      session[:user_id] = brewery[:id]
+      redirect "/login"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get "/login" do
+
+    erb :"/breweries/login"
+  end
+
+  post "/login" do
+
+    redirect "/breweries/#{@name.id}"
+  end
+
+
 
 end
