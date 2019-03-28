007b1d33b80ed1794a3d7055b109abfa927f41f0
yukijina <yukijina@gmail.com>
2019-03-27 20:55:25 -0700

    add session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :session, "wonderfulbrewery"
+
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
 
 end
