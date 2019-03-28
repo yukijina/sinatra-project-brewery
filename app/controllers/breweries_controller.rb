31ed36c7565efa06743d203151796ef461616e0b
yukijina <yukijina@gmail.com>
2019-03-28 07:03:57 -0700

    add login route, still working on

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -8,21 +8,39 @@ class BreweriesController < ApplicationController
     binding.pry
     brewery = Brewery.new(params)
     if brewery.save
-      session[:user_id] = brewery[:id]
-      redirect "/login"
+      session[:user_id] = brewery.id
+      redirect "/breweries"
     else
       redirect "/signup"
     end
   end
 
   get "/login" do
-
     erb :"/breweries/login"
   end
 
   post "/login" do
+    brewery = Brewery.find_by(email: params[:email])
 
-    redirect "/breweries/#{@name.id}"
+    if brewery && brewery.authenticate(params[:password])
+      session[:user_id] = brewery.id
+      redirect "/breweries/#{brewery.id}"
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/breweries/:id" do
+    if logged_in?
+      brewery = Brewery.find_by_id(params[:id])
+      current_user
+      erb :"/breweries/show"
+    else
+    end
+  end
+
+  get "/breweries" do
+    erb :"/breweries/breweries"
   end
 
 
