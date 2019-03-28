0b51fe854cc556d33875103f06986564718de85b
yukijina <yukijina@gmail.com>
2019-03-27 22:47:29 -0700

    create helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :session, "wonderfulbrewery"
-
+    enable :session, "wonderful_brewery"
   end
 
   get "/" do
     erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      Brewery.find_by(id: session[:user_id])
+    end
+
   end
 
 end
