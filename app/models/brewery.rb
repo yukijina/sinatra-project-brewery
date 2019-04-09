4bd19e151359c921b2f7f6e985105e43cf8155fc
yukijina <yukijina@gmail.com>
2019-04-09 15:26:18 -0700

    add validation uniqueness

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,4 +2,5 @@ class Brewery < ActiveRecord::Base
   has_many :brands
   has_secure_password
   validates :name, :email, :city, presence: true
+  validates :email, uniqueness: { message: " you entered is already taken. Please use another email address. OR please log in if you alerady have an account." }
 end
